fb38bd7d8ed687d24aff4d4c4cd445c84026d73f
unknown <henokmekuanint79@gmail.com>
2022-07-30 20:07:49 +0300

    stateadded

--- reactapp/src/component/counter.jsx
+++ reactapp/src/component/counter.jsx
@@ -2,28 +2,39 @@ import React,{Component} from 'react'
 class Coutner extends Component {
     state = { 
         count:0,
+        tags:['tag1','tag2','tag3']
      };
-     styles={
-        fontSize:100,
-        fontWeight:'bold'
-     }
+ handleelement=()=>{
+
+    console.log("Increment Clicked",this);
+ }
+     rendertags(){
+
+        if(this.state.tags.length===0) return<p> there are no tags</p>;
+        return  <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>
+           
+    }
     render() {
           
+
+
         return (<div>
-            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className='btn btn-secondary btn-sm'>Increment</button>
+             {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span> */}
+            <button onClick={this.handleelement()} className='btn btn-secondary btn-sm'>Increment</button> 
+            {this.state.tags.length===0 && "please create a new tag"}
+                {this.rendertags()}
             </div>);
     
     }
-    getBadgeClasses() {
-        let classes = "badg m-2 bg-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
+    // getBadgeClasses() {
+    //     let classes = "badg m-2 bg-";
+    //     classes += this.state.count === 0 ? "warning" : "primary";
+    //     return classes;
+    // }
 
-    formatCount(){
-        return this.state.count===0?"zero":this.state.count;
-    }
+    // formatCount(){
+    //     return this.state.count===0?"zero":this.state.count;
+    // }
 }
  
 export default Coutner; 
