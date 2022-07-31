fdb89195c6c6d2407daf166346f1c439615909d5
unknown <henokmekuanint79@gmail.com>
2022-07-31 18:12:10 +0300

    componentsareadded

--- reactapp/src/component/counter.jsx
+++ reactapp/src/component/counter.jsx
@@ -1,12 +1,11 @@
 import React,{Component} from 'react'
 class Coutner extends Component {
     state = { 
-        count:0,
+        value:this.props.value,
         tags:['tag1','tag2','tag3']
      };
  handleelement=()=>{
-
-    console.log("Increment Clicked",this);
+this.setState({value:this.state.value+1});
  }
      rendertags(){
 
@@ -15,26 +14,24 @@ class Coutner extends Component {
            
     }
     render() {
-          
-
 
         return (<div>
-             {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span> */}
-            <button onClick={this.handleelement()} className='btn btn-secondary btn-sm'>Increment</button> 
+             <span className={this.getBadgeClasses()}>{this.formatCount()}</span> 
+            <button onClick={this.handleelement} className='btn btn-secondary btn-sm'>Increment</button> 
             {this.state.tags.length===0 && "please create a new tag"}
                 {this.rendertags()}
             </div>);
     
     }
-    // getBadgeClasses() {
-    //     let classes = "badg m-2 bg-";
-    //     classes += this.state.count === 0 ? "warning" : "primary";
-    //     return classes;
-    // }
+    getBadgeClasses() {
+        let classes = "badg m-2 bg-";
+        classes += this.state.value === 0 ? "warning" : "primary";
+        return classes;
+    }
 
-    // formatCount(){
-    //     return this.state.count===0?"zero":this.state.count;
-    // }
+    formatCount(){
+        return this.state.value===0?"zero":this.state.value;
+    }
 }
  
 export default Coutner; 
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Coutner from './component/counter';
+import Coutners from './component/counters';
 import 'bootstrap/dist/css/bootstrap.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Coutner />
+    <Coutners />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring per formance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
