068ea1f17f312665539e7c821a1c64d79feae9b6
unknown <henokmekuanint79@gmail.com>
2022-08-01 23:16:38 +0300

    totalcounteradded

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Coutners from './component/counters';
+// import Coutners from './component/counters';
 import 'bootstrap/dist/css/bootstrap.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Coutners />
+    <App />
   </React.StrictMode>
 );
 
